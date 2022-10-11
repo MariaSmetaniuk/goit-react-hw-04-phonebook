3e707fb40fa9bab57a40f8afaef1a34f7ae81356
MariaSmetaniuk <smetaniukmaria@gmail.com>
2022-10-11 21:39:58 +0300

    update ContactList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,31 +1,26 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'components/Button/Button.styled';
 import { List, Item } from './ContactList.styled';
 
-export class ContactList extends Component {
-  render() {
-    const { contacts, onRemoveContact } = this.props;
-    if (contacts.length === 0) return;
-    return (
-      <List>
-        {contacts.map(({ name, id, number }) => (
-          <Item key={id}>
-            <span>{name}:</span> {number}
-            <Button
-              type="button"
-              onClick={() => {
-                onRemoveContact(id);
-              }}
-            >
-              Delete
-            </Button>
-          </Item>
-        ))}
-      </List>
-    );
-  }
-}
+export const ContactList = ({ contacts, onRemoveContact }) => {
+  return (
+    <List>
+      {contacts.map(({ name, id, number }) => (
+        <Item key={id}>
+          <span>{name}:</span> {number}
+          <Button
+            type="button"
+            onClick={() => {
+              onRemoveContact(id);
+            }}
+          >
+            Delete
+          </Button>
+        </Item>
+      ))}
+    </List>
+  );
+};
 
 ContactList.propTypes = {
   contacts: PropTypes.arrayOf(
